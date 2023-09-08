cc3de24ba923c89d0850c55ce4c289ab8b6b7cb5
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-07 19:37:27 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-int main (void)
+#include <stdio.h>
+
+/**
+* main - this is the main function
+*
+* Return: always 0
+*/
+
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle);
 return (0);
-
-
-
-
-
 }
