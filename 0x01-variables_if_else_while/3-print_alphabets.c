d549af6f01cc7e3fd24ad8399ff7c7f8cf654d82
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-11 19:25:18 -0700

    jkhd

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *Discription: 'char'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 97;
+int x = 65;
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+while (x <= 90)
+{
+putchar(x);
+x++;
+}
+
+putchar('\n');
+        return (0);
+}
