ff7de91b0ebfb157fe151861959947a5cb6215b0
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-11 19:52:11 -0700

    jhkj

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *Discription: 'num'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for(int i =0;i <=9; i++)
+{
+printf("%d", n);
+}return(0);
+}
