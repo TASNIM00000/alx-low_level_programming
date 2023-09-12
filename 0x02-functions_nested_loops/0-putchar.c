f267c20d88a337815cf7c07545921a1498feb6d2
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-12 15:54:05 -0700

    putt

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *Discription: 'task0'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+printf("_putchar");
+putchar('\n');
+        return (0);
+}
