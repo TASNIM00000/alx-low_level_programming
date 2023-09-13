486cbf1ab60e37e6d9b25a2fd778ec2d12358557
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-13 14:23:06 -0700

    jhk

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,9 @@
-#include <time.h>
-#include <stdio.h>
 #include "main.h"
-
 /**
- * _islower - check the  lowercase.
- * Description: print if it is lc
- * 0
- *
- * @c: The character in ASCII code
- *
- * Return: 1 for lowercase character . 0 for the rest.
- */
+*_islower - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
+*/
 int _islower(int c)
 {
 int i ;
@@ -25,4 +18,5 @@ else
 return (0);
 }
 }
+_putchar('\n');
 }
