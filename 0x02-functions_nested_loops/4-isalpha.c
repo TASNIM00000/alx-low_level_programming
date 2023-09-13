95aece1f97fc45dbf11f952ab465f275551d1dc6
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-13 15:02:00 -0700

    alp or not

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_islower - checkk if a character is in lowercase or upppc
+*_isalpha - checkk if a character is in lowercase or upppc
 *@c: character if it alphaaa
 *Return: return 0 or 1
 */
