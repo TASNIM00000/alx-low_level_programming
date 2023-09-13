f0237be2f9ea28249e9acfc2c803dfaefb7af42c
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-12 22:53:23 -0700

    alpha kterer

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ * Description: print alphaaa ten times.
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+int i;
+for(i = 0 ; i <= 10 ; i++)
+{
+int n = 97;
+while (n <= 122)
+{
+_putchar(n);
+n++;
+}
+putchar('\n');
+}
+}
+
