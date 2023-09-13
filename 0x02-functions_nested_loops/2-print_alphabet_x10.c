29c247638bc081aeead695b9c561848b14d05d37
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-12 22:56:20 -0700

    alpha10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase.
+ * print_alphabet_x10 - Prints the alphabet in lowercase.
  * Description: print alphaaa ten times.
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
 int i;
-for(i = 0 ; i <= 10 ; i++)
+for (i = 0 ; i <= 10 ; i++)
 {
 int n = 97;
 while (n <= 122)
