5942123128f580fdefe9a5a0bfdb4bdc880252ec
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-13 15:12:38 -0700

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_sign - checkk if a character is in lowercase
+*@n: integer if it  lc
+*Return: return 0 or 1 or -1
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return(1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else 
+_putchar('-');
+return('-1');
+}
