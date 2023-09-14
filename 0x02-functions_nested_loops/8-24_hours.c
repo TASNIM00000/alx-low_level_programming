614c76a248f4763d875dde66ce17d96f4c9a5a13
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-13 18:57:21 -0700

    jsd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
 *jack_bauer - checkk if a character is in lowercase
-*@c: character if it  lc
-*Return: return 0 or 1
+*desc
+*Return;
 */
 void jack_bauer(void)
 {
 int m;
 int h;
-for (h = 0 ; h <= 23 ; h ++)
+for (h = 0 ; h <= 23 ; h++)
 {
-for (m = 0 ; m <= 59 ; m ++)
+for (m = 0 ; m <= 59 ; m++)
 {
 _putchar((h / 10) + '0');
 _putchar((h % 10) + '0');
