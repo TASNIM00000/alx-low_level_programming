9e5fe055c51fcc53eef01a1da210b4ee24eb50f3
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-13 19:19:36 -0700

    print

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_to_98 - checkk if a character is in lowercase
+*@n: character if it  lc
+*Return: return 0 or 1
+*/
+void print_to_98(int n)
+{
+int i;
+for (i = n ; i <= 98 ; i++ )
+{
+_putchar('n');
+_putchar(',');
+_putchar(' ');
+}
+}
