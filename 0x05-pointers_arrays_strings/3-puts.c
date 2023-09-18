4c1faf427cac91b5079bd1f63fbbc44ea16ee472
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-18 15:48:19 -0700

    stri

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <time.h>
+#include <stdio.h>
+/**
+*_puts - checkk if a character is in lowercase
+*@str:pointer
+*Return: void
+*/
+void _puts(char *str)
+{
+puts(str);
+}
