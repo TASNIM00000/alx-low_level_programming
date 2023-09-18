3d14d3720df885d16ee777bb38c325727e9b8fed
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-18 14:21:54 -0700

    lengtjh

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+* _strlen - controls if a character is in lowercase
+*@s: character
+*Return: return the length
+*/
+int _strlen(char *s)
+{
+int l;
+l = strlen(s);
+return (l);
+}
