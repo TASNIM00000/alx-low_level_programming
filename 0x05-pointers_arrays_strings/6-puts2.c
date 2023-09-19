d988eff90d36f6f3f30652c6a1894d22c63aafc2
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-18 17:02:56 -0700

    lololo

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,19 +8,15 @@
 */
 void puts2(char *str)
 {
-	int len, i;
-
-	len = 0;
-
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-
-	for (i = 0; i < len; i += 2)
-	{
-	     putchar(str[i]);
-	}
-
-             putchar('\n');
+int len, i;
+len = 0;
+while (str[len] != '\0')
+{
+len++;
+}
+for (i = 0; i < len; i += 2)
+{
+putchar(str[i]);
+}
+putchar('\n');
 }
