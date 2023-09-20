7c6fe8ca456f0debe5dc332f02807f882b9ddf33
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-19 17:53:00 -0700

    copyy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+**_strcpy - checkk if a character is in lowercase
+*@dest: pointer 
+*@src: string
+*Return: void
+*/
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest , src);
+return (dest);
+}
