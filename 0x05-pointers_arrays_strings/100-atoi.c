875b6d557b28bb7323d471edd29daa693560285e
tasnimoo <tasnimsabry2003@gmail.com>
2023-09-19 18:03:21 -0700

    ddhgdjs

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* _atoi - checkk if 
+*@s: pointer
+*Return: integer numm
+*/
+int _atoi(char *s)
+{
+int i;
+i = atoi(s);
+return (i);
+}
