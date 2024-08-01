400d5657694911394234405494038d17f3aeb18b
TASNIM00000 <140080659+TASNIM00000@users.noreply.github.com>
2024-08-01 20:05:15 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+/**
+* main - this is the main function
+*
+* Return: always 0
+*/
 
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-return (0);}
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
